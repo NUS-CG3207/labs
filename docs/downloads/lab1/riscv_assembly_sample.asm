c9f405e6ff0dab5e43bf05d47c2839ca5de49773
rustyrisc <justforabot@gmail.com>
2025-08-30 01:21:44 +0800

    using .eqv for sample, changes to helloworld

--- docs/downloads/lab1/riscv_assembly_sample.asm
+++ docs/downloads/lab1/riscv_assembly_sample.asm
@@ -12,52 +12,23 @@
 
 # This sample program for RISC-V simulation using RARS
 
-# ------- <code memory (Instruction Memory ROM) begins>
-.text	## IROM segment: IROM_BASE to IROM_BASE+2^IROM_DEPTH_BITS-1
-# Total number of real instructions should not exceed 2^IROM_DEPTH_BITS/4 (127 excluding the last line 'halt B halt' if IROM_DEPTH_BITS=9).
-# Pseudoinstructions (e.g., li, la) may be implemented using more than one actual instruction. See the assembled code in the Execute tab of RARS
-
-# You can also use the actual register numbers directly. For example, instead of s1, you can write x9
-
-main:   
-	lw s0, MMIO_BASE			# MMIO_BASE
-	lw t1, LED_OFF
-	add s1, s0, t1		# LEDS address = MMIO_BASE + LED_OFF
-	lw t1, DIP_OFF
-	add s2, s0, t1		# DIPS address = MMIO_BASE + DIP_OFF
-loop:
-	lw s3, DELAY_VAL		# reading the loop counter value
-	lw s4, (s2)				# reading DIPS
-	sw s4, (s1)				# writing to LEDS
-
-wait:
-	addi s3, s3, -1		# subtract 1
-	beq s3, zero, loop	# exit the loop
-	j wait			# continue in the loop
-
-halt:	
-	jal halt		# infinite loop to halt computation. A program should not "terminate" without an operating system to return control to
-				# keep halt: jal halt as the last line of your code.
-				
-# ------- <code memory (Instruction Memory ROM) ends>			
-				
-				
-				
-					
-#------- <Data Memory begins>									
-.data  ## DR#----------------------------------------------------------------------------------
-#-- (c) Rajesh Panicker
-#--	License terms :
-#--	You are free to use this code as long as you
-#--		(i) DO NOT post it on any public repository;
-#--		(ii) use it only for educational purposes;
-#--		(iii) accept the responsibility to ensure that your implementation does not violate anyone's intellectual property.
-#--		(iv) accept that the program is provided "as is" without warranty of any kind or assurance regarding its suitability for any particular purpose;
-#--		(v) send an email to rajesh<dot>panicker<at>ieee.org briefly mentioning its use (except when used for the course CG3207 at the National University of Singapore);
-#--		(vi) retain this notice in this file and any files derived from this.
-#----------------------------------------------------------------------------------
-
-# This sample program for RISC-V simulation using RARS
+.eqv MMIO_BASE 0xFFFF0000
+# Memory-mapped peripheral register offsets
+.eqv UART_RX_VALID_OFF 			0x00 #RO, status bit
+.eqv UART_RX_OFF			0x04 #RO
+.eqv UART_TX_READY_OFF			0x08 #RO, status bit
+.eqv UART_TX_OFF			0x0C #WO
+.eqv OLED_COL_OFF			0x20 #WO
+.eqv OLED_ROW_OFF			0x24 #WO
+.eqv OLED_DATA_OFF			0x28 #WO
+.eqv OLED_CTRL_OFF			0x2C #WO
+.eqv ACCEL_DATA_OFF			0x40 #RO
+.eqv ACCEL_DREADY_OFF			0x44 #RO, status bit
+.eqv DIP_OFF				0x64 #RO
+.eqv PB_OFF				0x68 #RO
+.eqv LED_OFF				0x60 #WO
+.eqv SEVENSEG_OFF			0x80 #WO
+.eqv CYCLECOUNT_OFF			0xA0 #RO
 
 # ------- <code memory (Instruction Memory ROM) begins>
 .text	## IROM segment: IROM_BASE to IROM_BASE+2^IROM_DEPTH_BITS-1
@@ -66,33 +37,31 @@ halt:
 
 # You can also use the actual register numbers directly. For example, instead of s1, you can write x9
 
-main:   
-	lw s0, MMIO_BASE			# MMIO_BASE
-	lw t1, LED_OFF
-	add s1, s0, t1		# LEDS address = MMIO_BASE + LED_OFF
-	lw t1, DIP_OFF
-	add s2, s0, t1		# DIPS address = MMIO_BASE + DIP_OFF
+main:
+	li s0, MMIO_BASE		# MMIO_BASE. Implemented as lui+addi
+	# Could have done lw s0,MMIO_BASE (ARM style) instead of the li above, provided MMIO_BASE: .word 0xFFFF0000 was declared in the .data (DMEM) section instead of .eqv
+	addi s0, s0, LED_OFF		# LED address = MMIO_BASE + LED_OFF
+	li  s1, DIP_OFF			# note that this li doesn't translate to lui, unlike the li in line 41.
+	add s1, s0, s1			# DIP address = MMIO_BASE + DIP_OFF. Could have been done in a way similar to LED address, but done this way to have a DP reg instruction
 loop:
-	lw s3, DELAY_VAL		# reading the loop counter value
-	lw s4, (s2)				# reading DIPS
-	sw s4, (s1)				# writing to LEDS
+	lw s3, delay_val		# reading the loop counter value
+	lw s4, (s2)			# reading DIPS
+	sw s4, (s1)			# writing to LEDS
 
 wait:
 	addi s3, s3, -1		# subtract 1
 	beq s3, zero, loop	# exit the loop
-	j wait			# continue in the loop
+	jal zero, wait		# continue in the loop (could also have written j wait).
 
 halt:	
-	jal halt		# infinite loop to halt computation. A program should not "terminate" without an operating system to return control to
-				# keep halt: jal halt as the last line of your code.
+	j halt		# infinite loop to halt computation. A program should not "terminate" without an operating system to return control to
+				# keep halt: j halt as the last line of your code so that there is a 'dead end' beyond which execution will not proceed.
 				
 # ------- <code memory (Instruction Memory ROM) ends>			
 				
-				
-				
-					
+								
 #------- <Data Memory begins>									
-.data  ## DROM segment: DMEM_BASE to DMEM_BASE+2^DMEM_DEPTH_BITS-1
+.data  ## DMEM segment: DMEM_BASE to DMEM_BASE+2^DMEM_DEPTH_BITS-1
 # Total number of constants+variables should not exceed 2^DMEM_DEPTH_BITS/4 (128 if DMEM_DEPTH_BITS=9).
 # This kind of a data memory is slightly unrealistic and possible only in FPGAs. 
 # In embedded systems, the data memory has two parts - constants stored in a ROM/Flash (non-volatile) and variables stored in RAM. 
@@ -105,80 +74,16 @@ halt:
 
 DMEM:
 
-MMIO_BASE: .word 0xFFFF0000	# Should be the same as the .mmio address based on the Memory Configuration set in the assembler/linker, and Wrapper.v
-
-# Memory-mapped peripheral register offsets
-UART_RX_VALID_OFF: 		.word 0x00 #RO, status bit
-UART_RX_OFF: 			.word 0x04 #RO
-UART_TX_READY_OFF: 		.word 0x08 #RO, status bit
-UART_TX_OFF: 			.word 0x0C #WO
-OLED_COL_OFF: 			.word 0x20 #WO
-OLED_ROW_OFF: 			.word 0x24 #WO
-OLED_DATA_OFF: 			.word 0x28 #WO
-OLED_CTRL_OFF: 			.word 0x2C #WO
-ACCEL_DATA_OFF: 		.word 0x40 #RO
-ACCEL_DREADY_OFF: 		.word 0x44 #RO, status bit
-LED_OFF: 			.word 0x60 #WO
-DIP_OFF: 			.word 0x64 #RO
-PB_OFF: 			.word 0x68 #RO
-SEVENSEG_OFF: 			.word 0x80 #WO
-CYCLECOUNT_OFF: 		.word 0xA0 #RO
-
-# the above consumes 16 words = 64 bytes
-
-DELAY_VAL: .word 4	# a constant, at location DMEM+0x40
+delay_val: .word 4	# a constant, at location DMEM+0x00
 string1:
-.asciz "\r\nWelcome to CG3207...\r\n"	# string, at DMEM+0x44
-var1: .word	1 		# a statically allocated variable (which can have an initial value, here 1), at location DMEM+0x60
-# 100 bytes so far. Splitting the rest somewhat equally between heap and stack below.
-heap: .space 188	# heap memory - can be used for dynamic allocation. Not using RARS heap default address here. DMEM+0x64 to DMEM+0x11F
-stack: .space 224	# stack memory - can be used for function calls and local variables. DMEM+0x120 to DMEM+0x1FF.
-stack_top:	# stack pointer can be initialised to this location (i.e., the address of stack_top)
-				# stack grows downwards, so stack pointer should be decremented when pushing and incremented when popping
+.asciz "\r\nWelcome to CG3207..\r\n"	# string, from DMEM+0x4 to DMEM+0x1F (
+var1: .word	1 		# a statically allocated variable (which can have an initial value, say 1), at location DMEM+0x60
+# Food for thought: What will be the address of var1 if string1 had one extra character, say  "..." instead of ".."? Hint: words are word-aligned.
 
-#------- <Data Memory ends>									
-# DMEM segment: DMEM_BASE to DMEM_BASE+2^DMEM_DEPTH_BITS-1
-# Total number of constants+variables should not exceed 2^DMEM_DEPTH_BITS/4 (128 if DMEM_DEPTH_BITS=9).
-# This kind of a data memory is slightly unrealistic and possible only in FPGAs. 
-# In embedded systems, the data memory has two parts - constants stored in a ROM/Flash (non-volatile) and variables stored in RAM. 
-#   Constants are available for reading without a prior writing (and can't normally be written). 
-#	Variables that are initialized are explicitly set to their initial value via writes (sw instructions). Variables should't be read (lw) without a prior write (sw) somewhere.
-# In desktop systems, the data memory is typically all RAM, but has constants stored in read-only area that is initalised explicitly via writes (sw) and are not modified further.
-#   As with embedded systems, variables that are initialized are explicitly set to their initial value via writes (sw instructions). Variables should't be read (lw) without a prior write (sw) somewhere.
-# In contrast, the FPGA-based systems often have a more flexible memory architecture*, allowing for RAM to be used like ROM, i.e., variables are auto-initialised - can be read without prior writes, but can also be modified.
-#	*At least the way it is used in our case, which necessitates the memory to be small and implemented using block or distributed RAMs available within the FPGA fabric and initialised using GSR.
-
-DMEM:
-
-MMIO_BASE: .word 0xFFFF0000	# Should be the same as the .mmio address based on the Memory Configuration set in the assembler/linker, and Wrapper.v
-
-# Memory-mapped peripheral register offsets
-UART_RX_VALID_OFF: 		.word 0x00 #RO, status bit
-UART_RX_OFF: 			.word 0x04 #RO
-UART_TX_READY_OFF: 		.word 0x08 #RO, status bit
-UART_TX_OFF: 			.word 0x0C #WO
-OLED_COL_OFF: 			.word 0x20 #WO
-OLED_ROW_OFF: 			.word 0x24 #WO
-OLED_DATA_OFF: 			.word 0x28 #WO
-OLED_CTRL_OFF: 			.word 0x2C #WO
-ACCEL_DATA_OFF: 		.word 0x40 #RO
-ACCEL_DREADY_OFF: 		.word 0x44 #RO, status bit
-LED_OFF: 			.word 0x60 #WO
-DIP_OFF: 			.word 0x64 #RO
-PB_OFF: 			.word 0x68 #RO
-SEVENSEG_OFF: 			.word 0x80 #WO
-CYCLECOUNT_OFF: 		.word 0xA0 #RO
-
-# the above consumes 16 words = 64 bytes
-
-DELAY_VAL: .word 4	# a constant, at location DMEM+0x40
-string1:
-.asciz "\r\nWelcome to CG3207...\r\n"	# string, at DMEM+0x44
-var1: .word	1 		# a statically allocated variable (which can have an initial value, here 1), at location DMEM+0x60
-# 100 bytes so far. Splitting the rest somewhat equally between heap and stack below.
-heap: .space 188	# heap memory - can be used for dynamic allocation. Not using RARS heap default address here. DMEM+0x64 to DMEM+0x11F
-stack: .space 224	# stack memory - can be used for function calls and local variables. DMEM+0x120 to DMEM+0x1FF.
-stack_top:	# stack pointer can be initialised to this location (i.e., the address of stack_top)
-				# stack grows downwards, so stack pointer should be decremented when pushing and incremented when popping
-
-#------- <Data Memory ends>									
+.align 9	# To set the address at this point to be 512-byte aligned, i.e., DMEM+0x200
+STACK_INIT:	# Stack pointer can be initialised to this location - DMEM+0x200 (i.e., the address of stack_top)
+				# stack grows downwards, so stack pointer should be decremented when pushing and incremented when popping.
+				# stack can be used for function calls and local variables.
+			# Not allocating any heap, as it is unlikely to be used in this simple program. If we need dynamic memory allocation,
+				# we need to allocate heap and would typically use a heap manager.
+#------- <Data Memory ends>													
--- docs/downloads/lab2/HelloWorld.asm
+++ docs/downloads/lab2/HelloWorld.asm
@@ -13,24 +13,40 @@
 # This sample program prints "Welcome to CG3207" in response to "A\r" (A+Enter) received from Console. There should be a sufficient time gap between the press of 'A' and '\r'
 # if the processor is run at a low freq.
 
-.eqv LSB_MASK 0xFF
+.eqv LSB_MASK 0xFF # A mask to extract the least significant byte.
 
-# ------- <code memory (ROM mapped to Instruction Memory) begins>
-.text	## IROM segment 0x00000000-0x000001FC
-# Total number of instructions should not exceed 128 (127 excluding the last line 'halt B halt').
+.eqv MMIO_BASE 0xFFFF0000
+# Memory-mapped peripheral register offsets
+.eqv UART_RX_VALID_OFF 			0x00 #RO, status bit
+.eqv UART_RX_OFF			0x04 #RO
+.eqv UART_TX_READY_OFF			0x08 #RO, status bit
+.eqv UART_TX_OFF			0x0C #WO
+.eqv OLED_COL_OFF			0x20 #WO
+.eqv OLED_ROW_OFF			0x24 #WO
+.eqv OLED_DATA_OFF			0x28 #WO
+.eqv OLED_CTRL_OFF			0x2C #WO
+.eqv ACCEL_DATA_OFF			0x40 #RO
+.eqv ACCEL_DREADY_OFF			0x44 #RO, status bit
+.eqv DIP_OFF				0x64 #RO
+.eqv PB_OFF				0x68 #RO
+.eqv LED_OFF				0x60 #WO
+.eqv SEVENSEG_OFF			0x80 #WO
+.eqv CYCLECOUNT_OFF			0xA0 #RO
 
-# Note: see the wiki regarding the pseudoinstructions li and la. 
-# Pseudoinstructions may be implemented using more than one actual instruction. See the assembled code in the Execute tab of RARS
-# You can also use the actual register numbers directly. For example, instead of s6 (ABI name), you can write x22, but the former is preferred
+# ------- <code memory (Instruction Memory ROM) begins>
+.text	## IROM segment: IROM_BASE to IROM_BASE+2^IROM_DEPTH_BITS-1
+# Total number of real instructions should not exceed 2^IROM_DEPTH_BITS/4 (127 excluding the last line 'halt B halt' if IROM_DEPTH_BITS=9).
+# Pseudoinstructions (e.g., li, la) may be implemented using more than one actual instruction. See the assembled code in the Execute tab of RARS
 
-main:   
+main:
+	li s0, MMIO_BASE		# MMIO_BASE
 	li s6, LSB_MASK			# A mask for extracting out the LSB to check for '\0'
-	la s7, LEDS			# LEDs
-	la s8, CONSOLE_OUT_ready	# UART ready for output flag
-	la s9, CONSOLE_IN_valid		# UART new data flag
-	la s10, CONSOLE			# UART
-	li s11, 0x00002418		# SEVENSEG. Used li just to test lui
-	#la s11, SEVENSEG
+	addi s5, s0, SEVENSEG_OFF		# SEVENSEG.
+	addi s7, s0, LED_OFF		# LEDs
+	addi s8, s0, UART_TX_READY_OFF	# UART ready for output flag
+	addi s9, s0, UART_RX_VALID_OFF	# UART new data flag
+	addi s10, s0, UART_RX_OFF		# UART RX
+	addi s11, s0, UART_TX_OFF		# UART TX
 
 WAIT_A:
 	lw t1, (s9)		# read the new character flag
@@ -39,8 +55,8 @@ WAIT_A:
 ECHO_A:
 	lw t1, (s8)		# check if the UART is ready to be written
 	beqz t1, ECHO_A
-	sw t0, (s10)		# echo received character to the console
-	sw t0, (s11)		# show received character (ASCII) on the 7-Seg display
+	sw t0, (s11)		# echo received character to the console
+	sw t0, (s5)		# show received character (ASCII) on the 7-Seg display
 	sw t0, (s7)		# show received character (ASCII) on the LEDs
 	li t2, 'A'
 	bne t0, t2, WAIT_A	# not 'A'. Continue waiting
@@ -51,8 +67,8 @@ WAIT_CR:			# 'A' received. Need to wait for '\r' (Carriage Return - CR).
 ECHO_CR:
 	lw t1, (s8)		# check if the UART is ready to be written
 	beqz t1, ECHO_CR
-	sw t0, (s10)		# echo received character to the console
-	sw t0, (s11)		# show received character (ASCII) on the 7-Seg display
+	sw t0, (s11)		# echo received character to the console
+	sw t0, (s5)		# show received character (ASCII) on the 7-Seg display
 	sw t0, (s7)		# show received character (ASCII) on the LEDs
 	beq t0, t2, WAIT_CR 	# perhaps the user is trying again before completing the pervious attempt, or 'A' was repeated. Just a '\r' needed as we already got an 'A'
 	li t1, '\r'
@@ -61,14 +77,14 @@ ECHO_CR:
 	la a0, string1		# a0 stores the value to be displayed. This is the argument passed to PRINT_S
 PRINT_S:			# Call PRINT_S subroutine (not implemented as a subroutine for now as jal doesn't have link and jalr is not implemented)		
 	lw t0, (a0)		# load the word (4 characters) to be displayed
-	# sw t0, (s11)		# write to seven segment display
+	# sw t0, (s5)		# write to seven segment display
 	li t2, 4		# byte counter
 NEXTCHAR:
 	lw t1, (s8)		# check if CONSOLE is ready to send a new character
 	beqz t1, NEXTCHAR	# not ready, continue waiting
 	and t1, t0, s6 		# apply LSB_MASK
 	beqz t1, WAIT_A 	# null terminator ('\0') detected, done. Return to top
-	sw t1, (s10) 		# write to UART the Byte(4-t2) of the original word (composed of 4 characters) in (7:0) of the word to be written (remember, we can only write words, and LEDs/UART displays only (7:0) of the written word)
+	sw t1, (s11) 		# write to UART the Byte(4-t2) of the original word (composed of 4 characters) in (7:0) of the word to be written (remember, we can only write words, and LEDs/UART displays only (7:0) of the written word)
 	srli t0, t0, 8	 	# shift so that the next character comes into LSB
 	li t1, 1		# note : no subi instruction in RV
 	sub t2, t2, t1		# decrement the loop counter
@@ -76,55 +92,23 @@ NEXTCHAR:
 	addi a0, a0, 4		# point to next word (4 characters)
 	j PRINT_S		# start printing the next word
 halt:	
-	j halt			# infinite loop to halt computation. A program should not "terminate" without an operating system to return control to
-				# keep halt: j halt as the last line of your code, though not strictly necessary if there is an infinite loop somewhere.
+	j halt			# infinite loop to halt computation. 
+
+# ------- <code memory (Instruction Memory ROM) ends>			
 				
-# ------- <code memory (ROM mapped to Instruction Memory) ends>			
-				
-				
-				
-					
-#------- <constant memory (ROM mapped to Data Memory) begins>									
-.data	## DROM segment 0x00002000-0x000021FC
-# All constants should be declared in this section. This section is read only (Only lw, no sw).
-# Total number of constants should not exceed 128
-# If a variable is accessed multiple times, it is better to store the address in a register and use it rather than load it repeatedly.
-DROM:
+								
+#------- <Data Memory begins>									
+.data  ## DMEM segment: DMEM_BASE to DMEM_BASE+2^DMEM_DEPTH_BITS-1
+# Total number of constants+variables should not exceed 2^DMEM_DEPTH_BITS/4 (128 if DMEM_DEPTH_BITS=9).
+
+DMEM:
+
 string1:
 .asciz "\r\nWelcome to CG3207..\r\n"
 
-#------- <constant memory (ROM mapped to Data Memory) ends>									
-
-
-
-
-# ------- <variable memory (RAM mapped to Data Memory) begins>
-.align 9 ## DRAM segment. 0x00002200-0x000023FC #assuming rodata size of <= 512 bytes (128 words)
-# All variables should be declared in this section, adjusting the space directive as necessary. This section is read-write.
-# Total number of variables should not exceed 128. 
-# No initialization possible in this region. In other words, you should write to a location before you can read from it (i.e., write to a location using sw before reading using lw).
-DRAM:
-.space 512
-
-# ------- <variable memory (RAM mapped to Data Memory) ends>
-
-
-
-
-# ------- <memory-mapped input-output (peripherals) begins>
-.align 9 ## MMIO segment. 0x00002400-0x00002418
-MMIO:
-LEDS: .word 0x0			# 0x00002400	# Location simulating LEDs. //volatile unsigned int* LEDS = (unsigned int*)0x00002400#  
-DIPS: .word 0x0			# 0x00002404	# Location simulating DIP switches. //volatile unsigned int* DIPS = (unsigned int*)0x00002404#
-PBS: .word 0x0			# 0x00002408	# Location simulating Push Buttons. Used only in Lab 2
-SEVENSEG: .word	0x0		# 0x0000240C	# Location simulating 7-Segment LEDs. Used only in Lab 2 and later
-CONSOLE: .word 0x0		# 0x00002410	# Location simulating UART. Used only in Lab 2 and later
-CONSOLE_IN_valid: .word 0x0	# 0x00002414	# Location simulating UART input valid flag. Used only in Lab 2 and later
-CONSOLE_OUT_ready: .word 0x0	# 0x00002418	# Location simulating UART ready for output flag. Used only in Lab 2 and later
-
-# ------- <memory-mapped input-output (peripherals) ends>
-
-
+.align 9	# To set the address at this point to be 512-byte aligned, i.e., DMEM+0x200
+STACK_INIT:	# Stack pointer can be initialised to this location - DMEM+0x200 (i.e., the address of stack_top)
+#------- <Data Memory ends>													
 
 
 ########################### Ignore the code below #######################################
