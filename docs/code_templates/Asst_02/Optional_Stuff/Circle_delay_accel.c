a39627c0065bb0b68db9ddb9b2d8e5ab5084268c
Rajesh Panicker <86955414+r-panicker@users.noreply.github.com>
2025-09-22 10:55:41 +0800

    Update Circle_delay_accel.c

--- docs/code_templates/Asst_02/Optional_Stuff/Circle_delay_accel.c
+++ docs/code_templates/Asst_02/Optional_Stuff/Circle_delay_accel.c
@@ -5,7 +5,7 @@
 --	You are free to use this code as long as you
 --		(i) DO NOT post it on any public repository;
 --		(ii) use it only for educational purposes;
---		(iii) accept the responsibility to ensure that your implementation does not violate any intellectual property of ARM Holdings or other entities.
+--		(iii) accept the responsibility to ensure that your implementation does not violate any intellectual property of any entity;
 --		(iv) accept that the program is provided "as is" without warranty of any kind or assurance regarding its suitability for any particular purpose;
 --		(v) send an email to rajesh.panicker@ieee.org briefly mentioning its use (except when used for the course CG3207 at the National University of Singapore);
 --		(vi) retain this notice in this file or any files derived from this.
